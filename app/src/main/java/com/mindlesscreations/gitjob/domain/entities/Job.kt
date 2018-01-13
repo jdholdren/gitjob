6ebe624ee68fd0d8df49c2f34f67d56efdddda7e
jdholdren <jamesdholdren@gmail.com>
2018-01-13 02:15:48 -0500

    Model changes

--- app/src/main/java/com/mindlesscreations/gitjob/domain/entities/Job.kt
+++ app/src/main/java/com/mindlesscreations/gitjob/domain/entities/Job.kt
@@ -3,5 +3,10 @@ package com.mindlesscreations.gitjob.domain.entities
 /**
  * Represents a position, detailing the name, where it is, and the description
  */
-class Job {
-}
+class Job(
+        val id: String,
+        val title: String,
+        val location: String,
+        val company: String,
+        val description: String
+)
