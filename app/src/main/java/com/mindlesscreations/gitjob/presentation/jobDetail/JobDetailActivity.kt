acda354afb23911386e24856d5740cc3cdac28df
James Holdren <jamesdholdren@gmail.com>
2018-01-14 15:22:57 -0500

    Added animations for apply now button

--- app/src/main/java/com/mindlesscreations/gitjob/presentation/jobDetail/JobDetailActivity.kt
+++ app/src/main/java/com/mindlesscreations/gitjob/presentation/jobDetail/JobDetailActivity.kt
@@ -1,16 +1,26 @@
 package com.mindlesscreations.gitjob.presentation.jobDetail
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.widget.NestedScrollView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.animation.AccelerateDecelerateInterpolator
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.bumptech.glide.request.RequestOptions
 import com.mindlesscreations.gitjob.R
 import com.mindlesscreations.gitjob.domain.entities.Job
 import kotlinx.android.synthetic.main.activity_job_detail.*
+import java.util.*
 
 class JobDetailActivity : AppCompatActivity() {
+
+    private var applyAnimator: AnimatorSet? = null
+    private var applyHidden = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,20 +45,83 @@ class JobDetailActivity : AppCompatActivity() {
         this.supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
+    /**
+     * Sets the job properties on the needed fields in the correct views. Also sets up click listeners
+     * for the 'apply now' button
+     */
     private fun renderJob(job: Job) {
-        this.company.text = job.company
+        this.company_and_location.text = String.format(
+                Locale.US,
+                getString(R.string.company_and_location),
+                job.company,
+                job.location
+        )
         this.job_title.text = job.title
 
-        // TODO Set the location
-
-        // TODO Load the image
         Glide.with(this)
                 .load(job.companyLogo)
                 .apply(RequestOptions().centerCrop())
+                .transition(withCrossFade())
                 .into(this.image_background)
 
         // Set the description
         this.html_text_view.setHtml(job.description)
+
+        this.setupApplyButton(job)
+    }
+
+    private fun setupApplyButton(job: Job) {
+        // TODO Attach on click
+
+        this.scroll_view.setOnScrollChangeListener { _: NestedScrollView?, scrollX: Int, scrollY: Int, _: Int, oldScrollY: Int ->
+
+            // Check if it's moving down or near the bottom
+            if ((oldScrollY < scrollY && !this.applyHidden) || 25 > (this.scroll_view.bottom - (this.scroll_view.height + scrollY))) {
+                this.hideApply()
+            } else if (oldScrollY > scrollX && this.applyHidden) {
+                this.showApply()
+            }
+        }
+    }
+
+    private fun hideApply() {
+        this.applyHidden = true
+
+        val objectAnimator = ObjectAnimator.ofFloat(
+                this.apply_button,
+                "translationY",
+                this.apply_button.translationY,
+                200f
+                )
+        objectAnimator.duration = 225
+        objectAnimator.interpolator = AccelerateDecelerateInterpolator()
+
+        this.applyAnimator?.cancel()
+
+        this.applyAnimator = AnimatorSet()
+        this.applyAnimator?.play(objectAnimator)
+
+        this.applyAnimator?.start()
+    }
+
+    private fun showApply() {
+        this.applyHidden = false
+
+        val objectAnimator = ObjectAnimator.ofFloat(
+                this.apply_button,
+                "translationY",
+                this.apply_button.translationY,
+                0f
+        )
+        objectAnimator.interpolator = AccelerateDecelerateInterpolator()
+        objectAnimator.duration = 195
+
+        this.applyAnimator?.cancel()
+
+        this.applyAnimator = AnimatorSet()
+        this.applyAnimator?.play(objectAnimator)
+
+        this.applyAnimator?.start()
     }
 
     //endregion
